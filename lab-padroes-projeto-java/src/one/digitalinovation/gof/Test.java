aff37ecf49c396f1e48dbf9cb7d27b7813a8bdde
caioorleans <caioorleansdm@hotmail.com>
2022-08-05 14:22:27 -0300

    first commit

---
+++ lab-padroes-projeto-java/src/one/digitalinovation/gof/Test.java
@@ -0,0 +1,53 @@
+package one.digitalinovation.gof;
+
+import one.digitalinovation.gof.facade.Facade;
+import one.digitalinovation.gof.singleton.SingletonEager;
+import one.digitalinovation.gof.singleton.SingletonLazy;
+import one.digitalinovation.gof.singleton.SingletonLazyHolder;
+import one.digitalinovation.gof.strategy.Comportamento;
+import one.digitalinovation.gof.strategy.ComportamentoAgressivo;
+import one.digitalinovation.gof.strategy.ComportamentoDefensivo;
+import one.digitalinovation.gof.strategy.ComportamentoNormal;
+import one.digitalinovation.gof.strategy.Robo;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		//Testes relacionados ao padrão singleton
+		SingletonLazy lazy = SingletonLazy.getInstancia();
+		System.out.println(lazy);
+		lazy = SingletonLazy.getInstancia();
+		System.out.println(lazy);
+		
+		SingletonEager eager = SingletonEager.getInstancia();
+		System.out.println(eager);
+		eager = SingletonEager.getInstancia();
+		System.out.println(eager);
+		
+		SingletonLazyHolder holder = SingletonLazyHolder.getInstancia();
+		System.out.println(holder);
+		holder = SingletonLazyHolder.getInstancia();
+		System.out.println(holder);
+		
+		//Testes relacionados ao padrão strategy
+		Comportamento normal = new ComportamentoNormal();
+		Comportamento defensivo = new ComportamentoDefensivo();
+		Comportamento agressivo = new ComportamentoAgressivo();
+		
+		Robo r = new Robo();
+		r.setComportamento(normal);
+		r.mover();
+		
+		r.setComportamento(defensivo);
+		r.mover();
+		
+		r.setComportamento(agressivo);
+		r.mover();
+		
+		//Testes relacionados ao padrão facade
+		Facade facade = new Facade();
+		facade.migrarCliente("Caio", "70525470");
+	}
+
+}
---
+++ lab-padroes-projeto-java/src/one/digitalinovation/gof/singleton/SingletonEager.java
@@ -0,0 +1,18 @@
+package one.digitalinovation.gof.singleton;
+
+
+/**
+* Singleton "apressado"
+* @author caioorleans
+*/
+public class SingletonEager {
+	private static SingletonEager instancia = new SingletonEager();
+	
+	private SingletonEager() {
+		super();
+	}
+	
+	public static SingletonEager getInstancia() {
+		return instancia;
+	}
+}
